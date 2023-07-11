56e96049de09d0c6d056b1f4dc3a09ecb6bf657a
PiscitelloLuciano <lucianopiscitello8@gmail.com>
2023-07-11 18:36:41 -0300

    Formulario Terminado

--- src/app/lista-alumnos/alumnos/alumnos.component.ts
+++ src/app/lista-alumnos/alumnos/alumnos.component.ts
@@ -14,16 +14,16 @@ export class AlumnosComponent {
   constructor() {}
 
   alumnos: IAlumno[] = [
-    { nombre: 'Pedro Perez', nota: 6 },
-    { nombre: 'Juana Gonzalez', nota: 9 },
-    { nombre: 'Matias Semeria', nota: 7 },
+    { nombre: 'Pedro Perez', nota: 3 },
+    { nombre: 'Juana Gonzalez', nota: 2 },
+    { nombre: 'Matias Semeria', nota: 2 },
     { nombre: 'Lautaro Cortez', nota: 4 },
     { nombre: 'Joan Paez', nota: 2 },
-    { nombre: 'Luz Flores', nota: 7 },
+    { nombre: 'Luz Flores', nota: 1 },
     { nombre: 'Jazmin Carrera', nota: 3 },
-    { nombre: 'Osvaldo Ojeda ', nota: 10 },
-    { nombre: 'Ornelia Ojeda', nota: 10 },
-    { nombre: 'Agustin Marcuzzi', nota: 7 },
+    { nombre: 'Osvaldo Ojeda ', nota: 6 },
+    { nombre: 'Ornelia Ojeda', nota: 5 },
+    { nombre: 'Agustin Marcuzzi', nota: 1 },
   ];
 
   aprobado: boolean = false;
